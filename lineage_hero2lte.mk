2ee2d46d59a9f051a7f950e5f8b3304d5ee5011d
ivanmeler <i_ivan@windowslive.com>
2018-09-11 14:18:22 -0700

    Update build fingerprint

--- lineage_hero2lte.mk
+++ lineage_hero2lte.mk
@@ -33,3 +33,9 @@ PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hero2ltexx \
+    PRIVATE_BUILD_DESC="hero2ltexx-user 8.0.0 R16NW G935FXXU2ERD5 release-keys"
+
+BUILD_FINGERPRINT := samsung/hero2ltexx/hero2lte:8.0.0/R16NW/G935FXXU2ERD5:user/release-keys
