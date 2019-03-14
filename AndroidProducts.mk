33d084caedc27e3e96165cc863c8ed88d510164a
Luke Williams <lukewilliamsdl@gmail.com>
2019-03-14 01:50:14 +0000

    Prepare for PixelExperience OFficial

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_hero2lte.mk \
 	$(LOCAL_DIR)/aosp_hero2lte.mk \
-	$(LOCAL_DIR)/cosp_hero2lte.mk \
-	$(LOCAL_DIR)/du.mk \
-	$(LOCAL_DIR)/gzosp_hero2lte.mk \
-	$(LOCAL_DIR)/havoc_hero2lte.mk \
-	$(LOCAL_DIR)/pixeldust_hero2lte.mk \
-	$(LOCAL_DIR)/rr_hero2lte.mk \
-	$(LOCAL_DIR)/viper_hero2lte.mk \
-	$(LOCAL_DIR)/zen_hero2lte.mk \
-	$(LOCAL_DIR)/revengeos_hero2lte.mk \
-	$(LOCAL_DIR)/zirconium_hero2lte.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_hero2lte-eng
-add_lunch_combo lineage_hero2lte-userdebug
-add_lunch_combo lineage_hero2lte-user
+add_lunch_combo aosp_hero2lte-eng
+add_lunch_combo aosp_hero2lte-userdebug
+add_lunch_combo aosp_hero2lte-user
